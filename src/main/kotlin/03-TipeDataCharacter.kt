95d68983635f66fbadcf6ac46d18c9107154526d
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-08-26 00:10:33 +0700

    Tipe Data

---
+++ src/main/kotlin/03-TipeDataCharacter.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var a: Char= 'A'
+    var b: Char= 'B'
+    var char: Char= 'I'
+
+    print(a)
+    print(b)
+    print(char)
+}
---
+++ src/main/kotlin/05-TipeDataString.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name: String= "Desta"
+
+    //multi lane
+    var address: String="""
+        Purwokerto,
+        Cantral Java,
+        Indonesia.
+        """
+
+    println(name)
+    println(address)
+}
